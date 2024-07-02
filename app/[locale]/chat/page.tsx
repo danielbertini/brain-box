defe932fcf27b20be544cc3318983e08c77bc6b9
Daniel Bertini <danielbertini@gmail.com>
2024-07-02 20:33:59 -0300

    melhorias...

--- app/[locale]/chat/page.tsx
+++ app/[locale]/chat/page.tsx
@@ -2,11 +2,52 @@
 import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 import UIButton from "@/ui/button";
-import { ChevronLeft, Ellipsis } from "lucide-react";
+import { ChevronLeft, Ellipsis, SendHorizontal } from "lucide-react";
+import UITextField from "@/ui/textField";
+import UICard from "@/ui/card";
 
 export default function ChatPage() {
   const router = useRouter();
+  const tApp = useTranslations("App");
   const tChat = useTranslations("Chat");
+  const tForm = useTranslations("Form");
+
+  const renderInstructions = () => {
+    return (
+      <>
+        <span className="text-3xl font-bold text-secondary-500 my-12 w-full text-center">
+          {tApp("Name")}
+        </span>
+        <div className="flex flex-col gap-4 w-full">
+          <UICard
+            variant="flat"
+            className="text-secondary-500 p-4 w-full text-sm text-center text-balance">
+            {tChat("Instructions.1")}
+          </UICard>
+          <UICard
+            variant="flat"
+            className="text-secondary-500 p-4 w-full text-sm text-center text-balance">
+            {tChat("Instructions.2")}
+          </UICard>
+          <UICard
+            variant="flat"
+            className="text-secondary-500 p-4 w-full text-sm text-center text-balance">
+            {tChat("Instructions.3")}
+          </UICard>
+          <UICard
+            variant="flat"
+            className="text-secondary-500 p-4 w-full text-sm text-center text-balance">
+            {tChat("Instructions.5")}
+          </UICard>
+          <UICard
+            variant="flat"
+            className="text-secondary-500 p-4 w-full text-sm text-center text-balance">
+            {tChat("Instructions.5")}
+          </UICard>
+        </div>
+      </>
+    );
+  };
 
   return (
     <div className="p-8 flex flex-col items-center justify-between h-full gap-4">
@@ -37,11 +78,16 @@ export default function ChatPage() {
           </UIButton>
         </div>
       </div>
-      <div className="flex-1 flex items-center justify-between bg-secondary-500 w-full">
-        teste
+      <div className="flex-1 flex flex-col items-center justify-start w-full overflow-y-auto">
+        {renderInstructions()}
       </div>
-      <div className="flex-none flex items-center justify-between bg-secondary-500 h-12 w-full">
-        teste
+      <div className="flex-none flex items-center justify-between h-12 w-full">
+        <UITextField
+          placeHolder={tForm("SendAMessage")}
+          className="rounded-r-none"></UITextField>
+        <UIButton sizeType="icon" className="rounded-l-none">
+          <SendHorizontal />
+        </UIButton>
       </div>
     </div>
   );
