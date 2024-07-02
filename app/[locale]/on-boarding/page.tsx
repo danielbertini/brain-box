24fba7d31a8aaff1bb9c4f6cf8b7ffd14e7261f8
Daniel Bertini <danielbertini@gmail.com>
2024-07-02 17:12:45 -0300

    melhorias...

--- app/[locale]/on-boarding/page.tsx
+++ app/[locale]/on-boarding/page.tsx
@@ -45,83 +45,83 @@ export default function OnBoardingPage() {
           <UiLanguageToggle />
         </div>
       </div>
-      <div>
-        <div className="relative flex flex-col items-center w-full">
-          <div className="relative sm:w-[362px] w-[100%] sm:h-[400px] h-auto aspect-square rounded-3xl overflow-hidden z-20">
-            <Image
-              alt="Astronaut"
-              src="/images/astronaut.png"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <div className="absolute sm:w-[362px] w-[100%] sm:h-[400px] h-auto aspect-square rounded-3xl blur-2xl top-4 z-10">
-            <Image
-              alt="Astronaut"
-              src="/images/astronaut.png"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <div className="flex items-center justify-center gap-4 mt-9">
-            {Array.from({ length: slides.length }, (_, index) => {
-              return (
-                <div
-                  key={index}
-                  className={` rounded-full ${
-                    currentItem === index
-                      ? "bg-neutral-900 dark:bg-neutral-50 w-4 h-4 outline outline-offset-2 outline-1"
-                      : "bg-neutral-500 w-3 h-3"
-                  }`}
-                />
-              );
-            })}
-          </div>
-          <div
-            ref={area2scroll}
-            className="flex snap-x snap-mandatory gap-x-6 overflow-x-hidden no-scrollbar sm:w-[362px] w-[100%]">
-            {slides.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className="text-center text-balance sm:w-[362px] w-[100%] space-y-2 mt-6 snap-center flex-shrink-0">
-                  <h1 className="text-3xl font-bold text-secondary-900 dark:text-secondary-50 leading-10">
-                    {item.title}
-                  </h1>
-                  <p className="text-secondary-500 leading-7">
-                    {item.description}
-                  </p>
-                </div>
-              );
-            })}
-          </div>
-          <div className="flex items-center justify-center gap-2 mt-6 rounded-xl shadow-2xl bg-neutral-50 dark:bg-neutral-800">
-            <UIButton
-              variant="link"
-              isDisabled={Number(currentItem) === 0}
-              onPress={() => {
-                area2scroll?.current?.scrollTo({
-                  left: area2scroll.current.scrollLeft - 362,
-                  behavior: "smooth",
-                });
-                setCurrentItem(Number(currentItem) - 1);
-              }}>
-              <MoveLeft className="w-5" />
-            </UIButton>
-            <div className="w-[1px] h-4 bg-secondary-900 dark:bg-secondary-50" />
-            <UIButton
-              variant="link"
-              isDisabled={Number(currentItem) === slides.length - 1}
-              onPress={() => {
-                area2scroll?.current?.scrollTo({
-                  left: area2scroll.current.scrollLeft + 320,
-                  behavior: "smooth",
-                });
-                setCurrentItem(Number(currentItem) + 1);
-              }}>
-              <MoveRight className="w-5" />
-            </UIButton>
-          </div>
+      <div className="relative flex flex-col items-center w-full">
+        <div className="relative sm:w-[362px] w-[100%] sm:h-[400px] h-auto aspect-square rounded-3xl overflow-hidden z-20">
+          <Image
+            alt="Astronaut"
+            src="/images/astronaut.png"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div className="absolute sm:w-[362px] w-[100%] sm:h-[400px] h-auto aspect-square rounded-3xl blur-2xl top-4 z-10">
+          <Image
+            alt="Astronaut"
+            src="/images/astronaut.png"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      </div>
+      <div className="relative flex flex-col items-center w-full">
+        <div className="flex items-center justify-center gap-4 mt-9">
+          {Array.from({ length: slides.length }, (_, index) => {
+            return (
+              <div
+                key={index}
+                className={` rounded-full ${
+                  currentItem === index
+                    ? "bg-neutral-900 dark:bg-neutral-50 w-4 h-4 outline outline-offset-2 outline-1"
+                    : "bg-neutral-500 w-3 h-3"
+                }`}
+              />
+            );
+          })}
+        </div>
+        <div
+          ref={area2scroll}
+          className="flex snap-x snap-mandatory gap-x-6 overflow-x-hidden no-scrollbar sm:w-[362px] w-[100%]">
+          {slides.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="text-center text-balance sm:w-[362px] w-[100%] space-y-2 mt-6 snap-center flex-shrink-0">
+                <h1 className="text-3xl font-bold text-secondary-900 dark:text-secondary-50 leading-10">
+                  {item.title}
+                </h1>
+                <p className="text-secondary-500 leading-7">
+                  {item.description}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex items-center justify-center gap-2 mt-6 rounded-xl shadow-2xl bg-neutral-50 dark:bg-neutral-800">
+          <UIButton
+            variant="link"
+            isDisabled={Number(currentItem) === 0}
+            onPress={() => {
+              area2scroll?.current?.scrollTo({
+                left: area2scroll.current.scrollLeft - 362,
+                behavior: "smooth",
+              });
+              setCurrentItem(Number(currentItem) - 1);
+            }}>
+            <MoveLeft className="w-5" />
+          </UIButton>
+          <div className="w-[1px] h-4 bg-secondary-900 dark:bg-secondary-50" />
+          <UIButton
+            variant="link"
+            isDisabled={Number(currentItem) === slides.length - 1}
+            onPress={() => {
+              area2scroll?.current?.scrollTo({
+                left: area2scroll.current.scrollLeft + 320,
+                behavior: "smooth",
+              });
+              setCurrentItem(Number(currentItem) + 1);
+            }}>
+            <MoveRight className="w-5" />
+          </UIButton>
         </div>
       </div>
     </div>
